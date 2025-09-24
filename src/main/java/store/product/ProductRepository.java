37a2614d4385bc776da0cda4a88fe4cca15f82e9
RicardolCarvalho <ricardolc2@al.insper.edu.br>
2025-09-24 13:30:17 -0300

    feat

---
+++ src/main/java/store/product/ProductRepository.java
@@ -0,0 +1,12 @@
+package store.product;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<ProductModel, String> {
+
+    ProductModel findByName(String name);
+}
